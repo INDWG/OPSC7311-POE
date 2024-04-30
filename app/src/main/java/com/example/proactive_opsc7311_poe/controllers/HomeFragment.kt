ad291867a083085ede06d0366430c63286ed680a
Gerard <104638929+st10245621@users.noreply.github.com>
2024-04-30 16:45:19 +0200

    Update HomeFragment.kt

--- app/src/main/java/com/example/proactive_opsc7311_poe/controllers/HomeFragment.kt
+++ app/src/main/java/com/example/proactive_opsc7311_poe/controllers/HomeFragment.kt
@@ -11,70 +11,149 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proactive_opsc7311_poe.R
 import com.example.proactive_opsc7311_poe.models.Workout
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.firestore
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class HomeFragment : Fragment(), OnWorkoutClickListener
-{
-    private val db = FirebaseFirestore.getInstance()
-    private val workouts: MutableList<Workout> = ArrayList()
+class HomeFragment : Fragment(), OnWorkoutClickListener {
+
+    private val db = Firebase.firestore
+
+    private val workouts = mutableListOf<Workout>()
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View?
-    {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
+
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewWorkouts)
-        val adapter = WorkoutAdapter(workouts, this)
-        recyclerView.setAdapter(adapter)
-        recyclerView.setLayoutManager(LinearLayoutManager(context))
+
+        // Assuming you have an adapter class named 'WorkoutAdapter'
+        val adapter = WorkoutAdapter(workouts,this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context) // or GridLayoutManager(context, numberOfColumns)
+
+        // Set the text for date progress
         val dateProgress = view.findViewById<TextView>(R.id.date_progress)
-        dateProgress.text = "Today's Progress - " + currentDate
-        readData(adapter)
+        dateProgress.text = "Todays Progress - ${getCurrentDate()}"
+
+        readData()
+
         return view
     }
 
-    private val currentDate: String
-        private get()
-        {
-            val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
-            return dateFormat.format(Date())
+    override fun onWorkoutClicked(workoutName: String) {
+        val fragment = ViewExercisesFragment().apply {
+            arguments = Bundle().apply {
+                putString("workout_name", workoutName)
+            }
         }
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
 
-    private fun readData(adapter: WorkoutAdapter)
-    {
-        val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null)
-        {
-            val userId = auth.currentUser!!.uid
-            db.collection("users").document(userId).collection("workouts").get()
-                .addOnSuccessListener { queryDocumentSnapshots: QuerySnapshot ->
-                    workouts.clear()
-                    for (snapshot in queryDocumentSnapshots.getDocuments())
-                    {
-                        val workout = snapshot.toObject(Workout::class.java)
-                        if (workout != null)
-                        {
-                            workouts.add(workout)
+    // Function to get current date in readable format
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        return dateFormat.format(Date())
+    }
+
+    private fun readData() {
+        val user = FirebaseAuth.getInstance().currentUser
+        // Simplified date format without time
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val currentDate = dateFormat.format(Date())
+        Log.w("readData", "Current Date " + currentDate)
+
+        user?.let { currentUser ->
+            val userId = currentUser.uid
+
+            db.collection("users").whereEqualTo("uid", userId).get()
+                .addOnSuccessListener { querySnapshot ->
+                    if (!querySnapshot.isEmpty) {
+                        // There should be only one document with the given UID
+                        val document = querySnapshot.documents[0]
+                        val userData = document.data
+
+                        val username = view?.findViewById<TextView>(R.id.username)
+
+                        val firstname = userData?.get("firstname") as? String ?: ""
+
+                        if (firstname.isNotEmpty()) {
+                            username?.text = firstname
+                        } else {
+                            username?.text = "User"
                         }
+
+                        val userDocRef = document.reference
+
+                        // Check if the "workouts" collection exists for the user
+                        userDocRef.collection("workouts").get()
+                            .addOnSuccessListener { workoutsSnapshot ->
+                                for (workoutDocument in workoutsSnapshot.documents) {
+                                    val workoutId = workoutDocument.getString("workoutID") ?: ""
+                                    val workoutProgress = workoutDocument.getLong("progress")?.toInt() ?: 0
+                                    val totalExercises = workoutDocument.getLong("total-exercises")?.toInt() ?: 0
+
+                                    // Check if the workout is completed, if so, skip adding it to the list
+                                    if (workoutProgress != totalExercises) {
+                                        // Query the 'exercises' subcollection
+                                        userDocRef.collection("workouts").document(workoutId)
+                                            .collection("exercises").get()
+                                            .addOnSuccessListener { exercisesSnapshot ->
+                                                if (!exercisesSnapshot.isEmpty) {
+                                                    for (exerciseDocument in exercisesSnapshot.documents) {
+                                                        val exerciseDate = exerciseDocument.getTimestamp("date")?.toDate()?.let { date ->
+                                                            dateFormat.format(date)
+                                                        }
+
+                                                        if (currentDate == exerciseDate.toString()) {
+                                                            val workoutName = workoutDocument.getString("name") ?: ""
+                                                            val workoutDescription = workoutDocument.getString("description") ?: ""
+
+                                                            val newWorkout = Workout(workoutId, workoutName, workoutDescription, workoutProgress, totalExercises)
+
+                                                            var exists = false
+
+                                                            for (workout in workouts)
+                                                            {
+                                                                if (workout.workoutID == newWorkout.workoutID)
+                                                                {
+                                                                    exists = true
+                                                                }
+                                                            }
+
+                                                            if (!exists)
+                                                            {
+                                                                workouts.add(newWorkout)
+                                                            }
+                                                        }
+                                                    }
+                                                    updateRecyclerView(workouts)
+                                                }
+                                            }
+                                    }
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle any errors
+                                Log.w("readData", "Error getting workouts: ", e)
+                            }
                     }
-                    adapter.notifyDataSetChanged()
-                }.addOnFailureListener { e: Exception? ->
-                    Log.e(
-                        "HomeFragment", "Error reading workouts", e
-                    )
                 }
         }
     }
 
-    override fun onWorkoutClicked(workoutID: String)
-    {
-        Log.d("HomeFragment", "Workout clicked: $workoutID")
-        // Add navigation or action handling here
+    private fun updateRecyclerView(workouts: List<Workout>) {
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerViewWorkouts)
+        val adapter = WorkoutAdapter(workouts, this)
+        recyclerView?.adapter = adapter
+        recyclerView?.layoutManager = LinearLayoutManager(context)
     }
 }
